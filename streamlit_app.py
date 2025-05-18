32230fd95002a4d6419b4c65811cc206af7b2102
Nier'seDev <nienachuraru@yahoo.com>
2025-05-18 20:41:55 +0800

    Checking Markdown

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Applied Cryptography | Final Project | Group 1")
+
+st.markdown(
+    "# Crypto-Gadget: Applied Cryptographic Application\n"
+    "By: Hamzah Ibarreta Cuadra,\n"
+    "Harold Salvador,\n"
+    "Jude Fajardo"
 )
