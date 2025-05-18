f7a32d926b38a2c884716932266fd015cbc49c4a
Nier'seDev <nienachuraru@yahoo.com>
2025-05-18 20:57:15 +0800

    Side Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,16 @@
 import streamlit as st
 
-st.title("Applied Cryptography | Final Project | Group 1")
+st.title("Applied Cryptography | FP | Group 1")
 
 st.markdown(
     "By: Hamzah Ibarreta Cuadra,\n"
     "Harold Salvador,\n"
     "Jude Fajardo"
 )
+menu_option = st.sidebar.radio("Select Mode", ["Symmetric", "Asymmetric", "hashing"])
+if menu_option == "Symmetric":
+    st.subheader("Symmetric Encryption")
+elif menu_option == "Asymmetric":
+    st.subheader("Asymmetric Encryption")
+elif menu_option == "hashing":
+    st.subheader("Hashing")
