b217bd8b9e7bf6492308df3cfda6e70ddd72bc5c
Nier'seDev <nienachuraru@yahoo.com>
2025-05-20 22:49:00 +0800

    modified Sidebar

--- pages/Hashing.py
+++ pages/Hashing.py
@@ -2,22 +2,29 @@ import streamlit as st
 import hashlib
 
 # Using the sidebar for text input
-user_input = st.sidebar.text_input("Enter text to hash:")
+with st.sidebar:
+    st.title("Input:")    
+    hash_type = st.selectbox("Select Hash", ["MD5", "SHA1", "SHA256"])
+    user_input = st.text_input("Enter text to hash:")
 
 if user_input:
-    # MD5 hash
-    md5_hash = hashlib.md5(user_input.encode()).hexdigest()
-    # SHA-1 hash
-    sha1_hash = hashlib.sha1(user_input.encode()).hexdigest()
-    # SHA-256 hash
-    sha256_hash = hashlib.sha256(user_input.encode()).hexdigest()
-    
-    st.subheader("Input Text")
-    st.write(user_input)
-    
-    st.subheader("Hashes")
-    st.write("**MD5:**", md5_hash)
-    st.write("**SHA-1:**", sha1_hash)
-    st.write("**SHA-256:**", sha256_hash)
+    if user_input == "MD5":  
+        md5_hash = hashlib.md5(user_input.encode()).hexdigest()
+        st.subheader("Input Text:")
+        st.write(user_input)
+        st.subheader("Hashed Text:") 
+        st.write("**MD5:**", md5_hash)
+    elif user_input == "SHA1":
+        sha1_hash = hashlib.sha1(user_input.encode()).hexdigest()
+        st.subheader("Input Text:")
+        st.write(user_input)
+        st.subheader("Hashed Text:") 
+        st.write("**SHA-1:**", sha1_hash)
+    elif user_input == "SHA256":
+        sha256_hash = hashlib.sha256(user_input.encode()).hexdigest()
+        st.subheader("Input Text:")
+        st.write(user_input)
+        st.subheader("Hashed Text:") 
+        st.write("**SHA-256:**", sha256_hash)
 else:
     st.info("Please enter text in the sidebar to see the hashes.")
