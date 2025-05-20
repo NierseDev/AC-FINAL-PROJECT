57b1d06bfc637d73412d0025e51a8b81eedbd382
Nier'seDev <nienachuraru@yahoo.com>
2025-05-20 22:10:18 +0800

    renamed app.py to home.py

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("CryptoGadget")
+st.subheader("By: Hamzah Ibarreta Cuadra, Harold Salvador, Jude Fajardo")
+
+st.sidebar.page_links()
+
+st.write("Welcome to CryptoGadget! Use the sidebar to navigate between Symmetric, Asymmetric, and Hashing modes.")
