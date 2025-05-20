96c3b6217f5b01998ad88154537a949ab14a0462
Nier'seDev <nienachuraru@yahoo.com>
2025-05-20 22:21:14 +0800

    removed an error

--- Home.py
+++ Home.py
@@ -3,6 +3,4 @@ import streamlit as st
 st.title("CryptoGadget")
 st.subheader("By: Hamzah Ibarreta Cuadra, Harold Salvador, Jude Fajardo")
 
-st.sidebar.page_links()
-
 st.write("Welcome to CryptoGadget! Use the sidebar to navigate between Symmetric, Asymmetric, and Hashing modes.")
