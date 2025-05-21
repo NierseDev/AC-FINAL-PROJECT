4fd59dc018dae637e08b09a6e72ca6df78d6f44d
Nier'seDev <nienachuraru@yahoo.com>
2025-05-21 15:11:20 +0800

    Added Markdown

--- Home.py
+++ Home.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("CryptoGadget")
 st.subheader("By: Hamzah Ibarreta Cuadra, Harold Salvador, Jude Fajardo")
 
-st.write("Welcome to CryptoGadget! Use the sidebar to navigate between Symmetric, Asymmetric, and Hashing modes.")
+st.markdown("### Welcome to CryptoGadget! Use the sidebar to navigate between Symmetric, Asymmetric, and Hashing modes.")
