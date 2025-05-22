5cbae8ff9f558d9f4654f3caed3d0a61a5a83059
Nier'seDev <nienachuraru@yahoo.com>
2025-05-22 16:10:24 +0800

    Fixed Home Page

--- Home.py
+++ Home.py
@@ -3,4 +3,7 @@ import streamlit as st
 st.title("CryptoGadget")
 st.subheader("By: Hamzah Ibarreta Cuadra, Harold Salvador, Jude Fajardo")
 
-st.markdown("### Welcome to CryptoGadget! Use the sidebar to navigate between Symmetric, Asymmetric, and Hashing modes.")
+st.markdown("### Welcome to CryptoGadget! Use the sidebar to navigate between " \
+"- Symmetric, " \
+"- Asymmetric, and" \
+"- Hashing modes.")
